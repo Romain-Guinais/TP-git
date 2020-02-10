b13f5c2787723ca0f843268ec3151b4cee24414c
Mickael POUPION <m.poupion@iia-laval.fr>
2020-02-10 11:29:57 +0100

    update header for new member 2

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,4 @@
- ### application.bat - 2020 - GUINAIS - Blanchard Allan ###
+### application.bat - 2020 - GUINAIS - Blanchard Allan - Poupion Mickael ###
 version= "2"
 build_hour= "11H00"
 echo "Build${build_hour}"
